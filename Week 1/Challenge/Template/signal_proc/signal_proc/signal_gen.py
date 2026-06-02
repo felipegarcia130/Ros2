1d0c31808d19e2b9d059fff8e35dd690a5e49958
felipegarcia130 <felisuper13@hotmail.com>
2026-06-01 23:18:45 -0600

    montecarlo con path planning completado

--- Week 1/Challenge/Template/signal_proc/signal_proc/signal_gen.py
+++ Week 1/Challenge/Template/signal_proc/signal_proc/signal_gen.py
@@ -1,18 +1,15 @@
 # Imports
 #Import your libraries here
-
-
 #Class Definition
 class SignalGenerator(Node):
     def __init__(self):
         super().__init__('signal_gen')
         #Write your code here
-
-
+        pass
 #Main
 def main(args=None):
         #Write your code here
-
+        pass
 #Execute Node
 if __name__ == '__main__':
     main()
--- Week 1/Challenge/Template/signal_proc/signal_proc/signal_proc.py
+++ Week 1/Challenge/Template/signal_proc/signal_proc/signal_proc.py
@@ -1,17 +1,16 @@
 # Imports
 #Import your libraries here
-
 #Class Definition
 class SignalProcessing(Node):
     def __init__(self):
         super().__init__('signal_proc')
-    #Write your code here
+        #Write your code here
+        pass
         
-
 #Main
 def main(args=None):
     #Write your code here
-
+    pass
 #Execute Node
 if __name__ == '__main__':
     main()
